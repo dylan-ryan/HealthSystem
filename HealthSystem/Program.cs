d3d499cd339e726a56ea7c4cc35fb3d59687e3a2
Dylan <dylanryan431@gmail.com>
2023-10-03 16:16:38 -0300

    TakeDamage Done

--- HealthSystem/Program.cs
+++ HealthSystem/Program.cs
@@ -1,15 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace HealthSystem
 {
     internal class Program
     {
+        static int health;
+        static string healthStatus;
+        static int shield;
+        static int lives;
+
         static void Main(string[] args)
         {
+            health = 100;
+            shield = 100;
+            lives = 3;
+
+            ShowHud();
+            TakeDamage(20);
+            ShowHud();
+            TakeDamage(30);
+            ShowHud();
+            TakeDamage(55);
+            ShowHud();
+            TakeDamage(100);
+            ShowHud();
+            TakeDamage(300);
+            ShowHud(); 
+            TakeDamage(300);
+            ShowHud();
+
+            Console.ReadKey(true);
+        }
+
+        static void TakeDamage(int damage)
+        {
+            Console.WriteLine("You take " + damage + " damage");
+
+            int shieldDamgage = Math.Min(shield, damage);
+            int healthDamage = Math.Min(health, damage - shieldDamgage);
+
+            shield -= shieldDamgage;
+            health -= healthDamage;
+
+            //range check health 0to100
+            if (health < 1)
+            {
+                Console.WriteLine("You died");
+
+                lives = lives - 1;
+                //set health to be 0
+                health = 100;
+                shield = 100;
+
+                //lives range check
+                if (lives <= 0)
+                {
+                    Console.WriteLine("GAME OVER");
+                    health = 0;
+                    shield = 0;
+                }
+
+
+            }
+
+        }
+
+        static void Heal(int hp)
+        { 
+            Console.WriteLine("You healed for " + hp + " HP");
+            health = health + hp;
+        }
+
+        static void RegenerateShield(int hp)
+        {
+
+        }
+
+        static void ShowHud()
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" Shield: " + shield + "% ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(" Health: " + health + "% ");
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(" Lives: " + lives + " ");
+            Console.ResetColor();
+
+            Console.WriteLine();
         }
     }
 }
